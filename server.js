3233a075909fc5cc8eb3ea3ef07086f3cb2bb2a6
Bulpet <kuimov.efim@gmail.com>
2013-11-30 14:01:04 +0200

    comm 1

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-var express = require('express');
-var fs = require('fs');
-var mustache = require('mustache');
+var express 		= require('express');
+var fs 				= require('fs');
+var mustache 		= require('mustache');
+var config      	= require('./config');
+var log             = require('./log')(module);
+var ModelFactory    = require('./api/model').ModelFactory;
 
 //var server = express.createServer();
 // express.createServer()  is deprecated. 
 var server = express(); // better instead
 
+
 server.configure(function(){
 	var oneDay = 86400000;		//cache static files
-  	server.use('/model', express.static(__dirname + '/model'));
+  	server.use('/controlers', express.static(__dirname + '/controlers'));
   	server.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+  	server.use(express.logger('dev')); // выводим все запросы со статусами в консоль
+  	server.use(express.json()); // стандартный модуль, для парсинга JSON в запросах
+  	server.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
 });
+
+
 
 var demoData = [{ // dummy data to display
 "name":"Steve Balmer",
@@ -48,4 +57,59 @@ server.get('/app/:slug', function(req,res){
 	
 	res.send(html);
 });
+
+server.get('/api/model', function(req, res) {
+    return ModelFactory.find(function (err, articles) {
+        if (!err) {
+            return res.send(articles);
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            return res.send({ error: 'Server error' });
+        }
+    });
+});
+
+server.get('/api/model/:id', function(req, res) {
+    return ArticleModel.findById(req.params.id, function (err, model) {
+        if(!model) {
+            res.statusCode = 404;
+            return res.send({ error: 'Not found' });
+        }
+        if (!err) {
+            return res.send({ status: 'OK', model:model });
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+            return res.send({ error: 'Server error' });
+        }
+    });
+});
+
+server.post('/api/model', function(req, res) {
+    var model = new ModelFactory({
+        title: req.body.title,
+        name: req.body.name,
+        description: req.body.description
+        //images: req.body.images
+    });
+
+    model.save(function (err) {
+        if (!err) {
+            log.info("article created");
+            return res.send({ status: 'OK', model:model });
+        } else {
+            console.log(err);
+            if(err.name == 'ValidationError') {
+                res.statusCode = 400;
+                res.send({ error: 'Validation error' });
+            } else {
+                res.statusCode = 500;
+                res.send({ error: 'Server error' });
+            }
+            log.error('Internal error(%d): %s',res.statusCode,err.message);
+        }
+    });
+});
+
 server.listen(process.env.PORT || 3000);
